9e5b70205eee3d1e1089a29a3aaac08499f2e442
Jeremy Chen <jeremychenczm@gmail.com>
2026-01-14 17:35:47 -0800

    Lab1-Construct Book and Person class and test them

---
+++ Labs/Lab1/src/test/java/BookTest.java
@@ -0,0 +1,39 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+  private Book testBook1;
+  private Book testBook2;
+  private Book testBook3;
+
+  @BeforeEach
+  void setUp() {
+      this.testBook1 = new Book("title1", "author1", 100);
+      this.testBook2 = new Book("title2", "author2", 200);
+      this.testBook3 = new Book("title3", "author3", 300);
+  }
+
+  @Test
+  void getTitle() {
+      Assertions.assertEquals("title1", this.testBook1.getTitle());
+      Assertions.assertEquals("title2", this.testBook2.getTitle());
+      Assertions.assertEquals("title3", this.testBook3.getTitle());
+  }
+
+  @Test
+  void getAuthor() {
+    Assertions.assertEquals("author1", this.testBook1.getAuthor());
+    Assertions.assertEquals("author2", this.testBook2.getAuthor());
+    Assertions.assertEquals("author3", this.testBook3.getAuthor());
+  }
+
+  @Test
+  void getPages() {
+      Assertions.assertEquals(100, this.testBook1.getPages());
+      Assertions.assertEquals(200, this.testBook2.getPages());
+      Assertions.assertEquals(300, this.testBook3.getPages());
+  }
+}
